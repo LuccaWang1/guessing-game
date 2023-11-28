f0342e5de5ebeaccfda143ad75206680780cedb4
Lucca Wang <luccawang@gmail.com>
2023-11-28 13:58:48 -0700

    Created a Function, and Ridiculed the User for a Guess Outside of the 1-100 range

--- game.py
+++ game.py
@@ -1,26 +1,33 @@
 import random
-computer_num = random.randint(1, 100)
 
-print("Hello, player!")
-name = input("What's your name? ")
-print("Hello " + name)
+def guessing_game_function():
 
-print("This is the random number from the computer: ")
-print(computer_num)
+    computer_num = random.randint(1, 100)
 
-count = 1
+    print("Hello, player!")
+    name = input("What's your name? ")
+    print("Hello " + name)
 
-while True:
-    user_guess = int(input("Guess a random number: "))
-    if user_guess > computer_num:
-        print("Your guess is too high, try again.")
-        count += 1
-    elif user_guess < computer_num:
-        print("Your guess is too low, try again.")
-        count += 1
-    else:
-        print(f"You guessed the right number! Congratulations! It took you {count} attempts.")
-        break
+    print("This is the random number from the computer: ")
+    print(computer_num)
+    count = 1
+
+    while True:
+        user_guess = int(input("Guess a random number: "))
+        if user_guess < 1 or user_guess > 100:
+            print("Can't believe you guessed that - that's out of the range that you can guess in! How could you!")
+            count += 1
+        elif user_guess > computer_num:
+            print("Your guess is too high, try again.")
+            count += 1
+        elif user_guess < computer_num:
+            print("Your guess is too low, try again.")
+            count += 1
+        else:
+            print(f"You guessed the right number! Congratulations! It took you {count} attempts.")
+            break
+
+guessing_game_function()
 
 # greet player-
 # get player name-
