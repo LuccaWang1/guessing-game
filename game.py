2d025a6153758736ceb8ad2025ee34470f1dbf25
Lucca Wang <luccawang@gmail.com>
2023-11-27 18:48:32 -0700

    Final Solution - Hooray

--- game.py
+++ game.py
@@ -1,6 +1,34 @@
-"""A number-guessing game."""
+import random
+computer_num = random.randint(1, 100)
 
-# Put your code here
 print("Hello, player!")
+name = input("What's your name? ")
+print("Hello " + name)
 
-name = input("Enter your name please")
+print("This is the random number from the computer: ")
+print(computer_num)
+
+count = 1
+
+while True:
+    user_guess = int(input("Guess a random number: "))
+    if user_guess > computer_num:
+        print("Your guess is too high, try again.")
+        count += 1
+    elif user_guess < computer_num:
+        print("Your guess is too low, try again.")
+        count += 1
+    else:
+        print(f"You guessed the right number! Congratulations! It took you {count} attempts.")
+        break
+
+# greet player-
+# get player name-
+# choose random number between 1 and 100-
+# repeat forever:-
+#     get guess-
+#     if guess is incorrect:-
+#         give hint-
+#         increase number of guesses
+#     else:
+#         congratulate player
