f1aa2f595051829187cc88cd4bce52d9ef5d4d22
Lucca Wang <luccawang@gmail.com>
2023-12-04 20:33:12 -0700

    Game completed - enjoy playing

--- game.py
+++ game.py
@@ -1,19 +1,26 @@
 import random
 
 def guessing_game_function():
-
+    """User guesses a number between 1-100, and the function prints statements according to the guesses."""
+    
     computer_num = random.randint(1, 100)
 
     print("Hello, player!")
+
     name = input("What's your name? ")
+    
     print("Hello " + name)
 
     print("This is the random number from the computer: ")
     print(computer_num)
+
+    print("At any time, press 'q' to quit this game.")
+    
     count = 1
 
     while True:
-        user_guess = int(input("Guess a random number: "))
+        user_guess = input("Guess a random number: ")
+        user_guess = int(user_guess)
         if user_guess < 1 or user_guess > 100:
             print("Can't believe you guessed that - that's out of the range that you can guess in! How could you!")
             count += 1
@@ -28,14 +35,3 @@ def guessing_game_function():
             break
 
 guessing_game_function()
-
-# greet player-
-# get player name-
-# choose random number between 1 and 100-
-# repeat forever:-
-#     get guess-
-#     if guess is incorrect:-
-#         give hint-
-#         increase number of guesses
-#     else:
-#         congratulate player
